4320ea5794a93eefecf7dcc0319a8376de1bacb6
bugling <baebuseung@gmail.com>
2026-01-27 19:18:29 +0900

    문서 검색 추가

---
+++ untitled/src/bbs/self/BOJ1543_1.java
@@ -0,0 +1,47 @@
+package bbs.self;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1543_1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] dictionary = br.readLine().toCharArray();
+        char[] text = br.readLine().toCharArray();
+
+        int index = 0;
+        int cnt = 0;
+        for (int i = 0; i < dictionary.length && index < dictionary.length;) {
+
+            index = i;
+            boolean matched = true;
+
+            for (int j = 0; j < text.length;) {
+
+                if (dictionary[index] != text[j]) {
+                    j = 0;
+                    i++;
+                    matched = false;
+                    break;
+                } else {
+                    j++;
+                    index++;
+                }
+
+                // 내부 비교용 인덱스가 전체 범위 밖으로 넘어가면 text내부 반복문 종료(ArrayIndexOutOfBoundsException 방지)
+                if (index == dictionary.length) {
+                    matched = false;
+                    break;
+                }
+            }
+
+            if (matched == true) {
+                cnt++;
+                i = index;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
