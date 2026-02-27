304e1bfb37cd8b3025c97f058e73603c76685d85
bugling <baebuseung@gmail.com>
2026-02-27 11:36:04 +0900

    선택정렬, 삽입정렬, 퀵정렬 추가

---
+++ untitled/src/bbs/basic/sort/InsertionSort.java
@@ -0,0 +1,44 @@
+package bbs.basic.sort;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+
+    static void swap(int[] a, int idx1, int idx2) {
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+    }
+
+    static void InsertionSort(int[] a, int n) {
+        for (int i = 1; i < n; i++) {
+            int j;
+            int tmp = a[i];
+            for (j = i; j > 0 && a[j-1] > tmp ; j--) {
+                a[j] = a[j - 1];
+            }
+            a[j] = tmp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("삽입 정렬(버전 1)");
+        System.out.print("요솟수 : ");
+        int nx = stdIn.nextInt();
+        int[] x = new int[nx];
+
+        for (int i = 0; i < nx; i++) {
+            System.out.print("x[" + i + "] = ");
+            x[i] = stdIn.nextInt();
+        }
+
+        InsertionSort(x, nx);
+
+        System.out.println("오름차순으로 정렬하겠습니다.");
+        for (int i = 0; i < nx; i++) {
+            System.out.println("x[" + i +"] = " + x[i]);
+        }
+    }
+}
